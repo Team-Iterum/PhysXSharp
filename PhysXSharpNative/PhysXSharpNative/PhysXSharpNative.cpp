c90c71c5e763f9ccca4037d1caf41957c085236e
MagistrAVSH <art046@hotmail.com>
2019-12-14 08:26:55 +0300

    update

---
+++ PhysXSharpNative/PhysXSharpNative/PhysXSharpNative.cpp
@@ -0,0 +1,17 @@
+﻿// PhysXSharpNative.cpp : Defines the entry point for the application.
+//
+
+#include "PhysXSharpNative.h"
+
+using namespace std;
+using namespace physx;
+
+int main()
+{
+	PxDefaultAllocator		gAllocator;
+	PxDefaultErrorCallback	gErrorCallback;
+	auto foundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+	
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ PhysXSharpNative/PhysXSharpNative/PhysXSharpNative.h
@@ -0,0 +1,10 @@
+﻿// PhysXSharpNative.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+
+#include <iostream>
+#include "PxPhysicsAPI.h"
+
+// TODO: Reference additional headers your program requires here.
